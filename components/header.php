c081a1fd196012904d916e94084992b97767dacc
Zimal <zimaltajwer@gmail.com>
2025-11-06 20:03:35 -0800

    foldersadd

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <header>
+  <nav>
+    <a href="index.php"><b>E-Book System</b></a>
+    <a href="books/index.php">Books</a>
+    <a href="competitions/index.php">Competitions</a>
+    <a href="cart/index.php">Cart</a>
+    <a href="auth/login.php">Login</a>
+  </nav>
+</header>
+
+</body>
+</html>
