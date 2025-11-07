473a603854cb8b05c28a39cd84aecc092a26b379
Zimal <zimaltajwer@gmail.com>
2025-11-06 20:11:13 -0800

    Update header.php

--- components/header.php
+++ components/header.php
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/index.css">
-</head>
-<body>
-    <header>
-  <nav>
-    <a href="index.php"><b>E-Book System</b></a>
-    <a href="books/index.php">Books</a>
-    <a href="competitions/index.php">Competitions</a>
-    <a href="cart/index.php">Cart</a>
-    <a href="auth/login.php">Login</a>
-  </nav>
-</header>
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Font Awesome CDN -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+  />
+  <!-- Your CSS -->
+  <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+  <nav>
+    <div class="logo">
+      <img src="./images/logo.png" alt="Logo">
+    </div>
+    <div class="links">
+      <a href="index.php"><i class="fa-solid fa-house"></i> Home</a>
+      <a href="pages/contact.php"><i class="fa-solid fa-envelope"></i> Contact Us</a>
+      <a href="pages/about.php"><i class="fa-solid fa-circle-info"></i> About Us</a>
+      <a href="pages/faq.php"><i class="fa-solid fa-question-circle"></i> FAQ / Help</a>
+      <a href="competitions/index.php"><i class="fa-solid fa-trophy"></i> Competitions List</a>
+      <a href="competitions/participate.php"><i class="fa-solid fa-pen-to-square"></i> Participate</a>
+      <a href="competitions/winners.php"><i class="fa-solid fa-crown"></i> Winners</a>
+    </div>
+  </nav>
 </body>
+
 </html>
